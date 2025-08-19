a4f938114767f5a02f23541caa37b08cf91616cc
YONGMUN01 <rladyd887@gmail.com>
2025-08-19 10:41:34 +0900

    250819 10:40 김용문 로그인 dto, dao, LoginOkController, MemberMapper, MemberFrontController 작업

--- Reb_back/src/main/java/com/sol/app/member/MemberFrontController.java
+++ Reb_back/src/main/java/com/sol/app/member/MemberFrontController.java
@@ -1,41 +1,68 @@
 package com.sol.app.member;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sol.app.Result;
+
 /**
  * Servlet implementation class MemberFrontController
  */
-@WebServlet("/MemberFrontController")
+//@WebServlet("/MemberFrontController")
 public class MemberFrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MemberFrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public MemberFrontController() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		doProcess(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doProcess(request, response);
+	}
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+
+		String target = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println("현재 경로 : " + target);
+		Result result = new Result();
+
+		switch(target) {
+		case "/member/login.me":
+			System.out.println("로그인 페이지 요청");
+			request.getRequestDispatcher("/app/member/login.jsp").forward(request, response);
+			break;
+		case "/member/loginOk.me":
+			System.out.println("로그인 처리 요청");
+			result = new LoginOkController().execute(request, response);
+			break;
+		}
 	}
 
 }
