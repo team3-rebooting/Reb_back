82982329ffab716d8603e88fa8e295c2a6e505f6
jh011228 <jh01122844@gmail.com>
2025-08-20 02:04:10 +0900

    250820 02:04 최장현 관리자 로그인 오류 수정

--- Reb_back/src/main/java/com/sol/app/admin/AdminFrontController.java
+++ Reb_back/src/main/java/com/sol/app/admin/AdminFrontController.java
@@ -1,10 +1,8 @@
 package com.sol.app.admin;
 
 import java.io.IOException;
-import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +13,7 @@ import com.sol.app.Result;
  * Servlet implementation class AdminFrontController
  */
 public class AdminFrontController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+   private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -25,47 +23,55 @@ public class AdminFrontController extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doProcess(request, response);
-	}
+   /**
+    * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doProcess(request, response);
+   }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doProcess(request, response);
-	}
-	  protected void doProcess(HttpServletRequest request, HttpServletResponse response)
-		         throws ServletException, IOException {
-		      request.setCharacterEncoding("UTF-8");
-		      response.setCharacterEncoding("UTF-8");
+   /**
+    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+    */
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      // TODO Auto-generated method stub
+      doProcess(request, response);
+   }
+     protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+               throws ServletException, IOException {
+            request.setCharacterEncoding("UTF-8");
+            response.setCharacterEncoding("UTF-8");
 
-		      String target = request.getRequestURI().substring(request.getContextPath().length());
-		      Result result = new Result();
-		      
-		      switch(target) {
-		      		case "/admin/login.ad" :
-		      		request.getRequestDispatcher("/app/admin/login/admin-login.jsp").forward(request, response);
-		      		break;
-		      		case "/admin/loginOk.ad" :
-		      		System.out.println("여까지가나?");
-		      		result = new LoginOkController().execute(request, response);
-		      		break;
-		      }
-		      
-		      if(result != null) {
-					if(result.isRedirect()) {
-						response.sendRedirect(result.getPath());
-					}else {
-						request.getRequestDispatcher(result.getPath()).forward(request, response);
-					}
-				}
-		      
-		      
-	  }
+            String target = request.getRequestURI().substring(request.getContextPath().length());
+            Result result = new Result();
+            
+            switch(target) {
+               case "/admin/login.ad" :
+                  System.out.println("login으로 가야됨");
+                  request.getRequestDispatcher("/app/admin/login/admin-login.jsp").forward(request, response);
+                  System.out.println("login에서 돌아옴");
+                  break;
+               case "/admin/loginOk.ad" :
+                  System.out.println("로그인오케이 가야됨");
+                  result = new LoginOkController().execute(request, response);
+                  System.out.println("로그인오케이 돌아옴");
+                  break;
+               case "/admin/noticeList.ad" :
+                  System.out.println("공지사항 리스트");
+                  request.getRequestDispatcher("/app/admin/notice/admin-notice-list.jsp");
+                  break;
+                  
+            }
+            
+            if (result != null && result.getPath() != null) {
+            if (result.isRedirect()) {
+               response.sendRedirect(result.getPath());
+            } else {
+               request.getRequestDispatcher(result.getPath()).forward(request, response);
+            }
+         }
+            
+            
+     }
 }
