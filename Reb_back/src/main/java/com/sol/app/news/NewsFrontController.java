0a5499564f42567c7510578ae54c24d121c41000
MinGyeongSeung <rudtmd92@gmail.com>
2025-08-21 06:58:05 +0900

    공지사항 리스트 DTO, Mapper, Dao, FrontController, 메인페이 DTO 완성

--- Reb_back/src/main/java/com/sol/app/news/NewsFrontController.java
+++ Reb_back/src/main/java/com/sol/app/news/NewsFrontController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sol.app.Result;
+
 /**
  * Servlet implementation class NewsFrontController
  */
@@ -34,8 +36,32 @@ public class NewsFrontController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String target = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println("NewsFrontController 현재 경로 : " + target);
+		Result result = new Result();
+		
+		//web.xml에서 아직 정해지지 않아 임의로 지정
+		switch(target) {
+		case "/notice/noticeListOk.no":
+			System.out.println("공시사항 게시물 목록 처리 요청");
+			//임시로 이동처리
+//			request.getRequestDispatcher("/app/notice/notice-list.jsp").forward(request, response);
+			result = new noticeListOkController().execute(request, response);
+			System.out.println("result");
+			break;
+		
+		case "/notice/noticeReadOk.no":
+			System.out.println("게시물 상세 페이지 처리 요청");
+			break;
+		}
+		
+		if(result != null) {
+			if(result.isRedirect()) {
+				response.sendRedirect(result.getPath());
+			} else {
+				request.getRequestDispatcher(result.getPath()).forward(request, response);
+			}
+		}
 	}
 
 }
