749ad3dd1ef0a993d8ef2f5868b6e762477d8efc
YONGMUN01 <rladyd887@gmail.com>
2025-08-23 05:20:06 +0900

    250823 05:19 김용문 수업 후기 목록 작업 완료

--- Reb_back/src/main/java/com/sol/app/course/CourseFrontController.java
+++ Reb_back/src/main/java/com/sol/app/course/CourseFrontController.java
@@ -1,16 +1,18 @@
 package com.sol.app.course;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sol.app.Result;
+
 /**
  * Servlet implementation class CourseFrontController
  */
-@WebServlet("/CourseFrontController")
+//@WebServlet("/CourseFrontController")
 public class CourseFrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -39,7 +41,25 @@ public class CourseFrontController extends HttpServlet {
 	}
 	
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		String target = request.getRequestURI().substring(request.getContextPath().length());
+		System.out.println("CourseFrontController 현재 경로" + target);
+		Result result = new Result();
 		
+		switch(target) {
+		case "/course/courseReviewListOk.cr":
+			System.out.println("수업 후기 목록 처리 요청");
+			result = new CourseReviewListOkController().execute(request, response);
+			break;
+		case "/course/courseReviewDetailOk.cr":
+			System.out.println("수업 후기 상세 페이지 요청");
+			result = new CourseReviewDetailOkController().execute(request, response);
+		}
+		
+		if(result != null) {
+			response.sendRedirect(result.getPath());
+		} else {
+			request.getRequestDispatcher(result.getPath()).forward(request, response);
+		}
 		
 	}
 
--- Reb_back/src/main/java/com/sol/app/course/dao/CourseReviewDAO.java
+++ Reb_back/src/main/java/com/sol/app/course/dao/CourseReviewDAO.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
+import com.sol.app.dto.CourseReviewDTO;
 import com.sol.app.dto.CourseReviewListDTO;
 import com.sol.config.MyBatisConfig;
 
@@ -16,8 +17,46 @@ public class CourseReviewDAO {
 	}
 	
 	public List<CourseReviewListDTO> selectAll(Map<String, Integer> pageMap){
+		System.out.println("모든 게시글 조회하기" + pageMap);
+		List<CourseReviewListDTO> list = sqlSession.selectList("courseReview.selectAll", pageMap);
+		System.out.println("조회 결과 : " + list);
 		
-		return null;
+		return list;
+	}
+	
+	public int getTotal() {
+		System.out.println("게시글 총 개수 조회");
+		
+		return sqlSession.selectOne("courseReview.getTotal");
+	}
+	
+	// 수업 후기 생성 후 파일 테이블에서 사용하기 위한 수업 후기 번호 반환
+	public int insertCourseReview(CourseReviewDTO courseReviewDTO) {
+		int insert = sqlSession.insert("courseReview.insert", courseReviewDTO);
+		System.out.println(courseReviewDTO + "출력");
+		System.out.println("수업 후기 생성");
+		System.out.println("insert 결과 : " + insert);
+		System.out.println("생성된 수업 후기 번호 : " + courseReviewDTO.getCourseReviewNumber());
+		
+		return courseReviewDTO.getCourseReviewNumber();
 	}
 
+	// 수업 후기 삭제
+	public void deleteRequest(int courseReviewNumber) {
+		System.out.println("수업 후기 삭제 : " + courseReviewNumber);
+		sqlSession.delete("courseReview.deleteRequest", courseReviewNumber);
+		System.out.println("수업 후기 삭제 완료");
+	}
+	
+	// 수업 후기 수정
+	public void updateRequest(CourseReviewDTO courseReviewDTO) {
+		System.out.println("수업 후기 수정 : " + courseReviewDTO.getCourseReviewNumber());
+		sqlSession.update("courseReview.update", courseReviewDTO);
+	}
+	
+	// 수업 후기 상세 페이지 조회
+	public CourseReviewListDTO select(int courseReviewNumber) {
+		System.out.println("수업 후기 상세 페이지 조회(단건 조회)");
+		return sqlSession.selectOne("courseReview.select", courseReviewNumber);
+	}
 }
