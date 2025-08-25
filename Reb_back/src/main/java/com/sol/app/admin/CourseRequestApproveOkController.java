46e67bb56003b4ecf4fe81e20138c9f43b740a9a
jh011228 <jh01122844@gmail.com>
2025-08-25 13:37:29 +0900

    250825 13:37 최장현 수업 승인 진행중

---
+++ Reb_back/src/main/java/com/sol/app/admin/CourseRequestApproveOkController.java
@@ -0,0 +1,22 @@
+package com.sol.app.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sol.app.Execute;
+import com.sol.app.Result;
+
+public class CourseRequestApproveOkController implements Execute{
+
+	@Override
+	public Result execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Result result = new Result();
+		
+		return null;
+	}
+
+}
