580649960623940a5a9de3f92c63769fdadcc89d
YONGMUN01 <rladyd887@gmail.com>
2025-09-15 09:13:51 +0900

    250915 09:13 카카오 api 연동

--- Reb_back/src/main/webapp/assets/js/auth/kakao-login-api.js
+++ Reb_back/src/main/webapp/assets/js/auth/kakao-login-api.js
@@ -17,8 +17,8 @@ window.addEventListener('DOMContentLoaded', () => {
 						const userId = response.id;
 						// 비밀번호
 						const userEmail = response.kakao_account.email;
-						const userNmae = response.kakao_account.name;
-						const userGender = null;
+						const userName = response.kakao_account.name;
+						let userGender;
 						if(response.kakao_account.gender === 'male'){
 							userGender = 'M';
 						}
@@ -27,15 +27,39 @@ window.addEventListener('DOMContentLoaded', () => {
 						}
 						const userNickname = response.properties.nickname;
 						const userPhone = response.kakao_account.phone_number;
+						const userBirth = response.kakao_account.birthyear + "-" + response.kakao_account.birthday.substr(0, 2) + 
+											"-" + response.kakao_account.birthday.substr(2, 2);
 						
 						const user = {
 							memberId : userId,
+							memberPassword : 'a',
+							zipCode : '',
+							address : '',
 							memberEmail : userEmail,
 							memberName : userName,
 							memberGender : userGender,
 							memberNickname : userNickname,
-							memberPhoneNumber : userPhone
+							memberPhoneNumber : userPhone,
+							memberBirthDay : userBirth
 						};
+						
+						console.log(user);
+						
+						/*fetch(`/member/checkIdOk.me?memberId=${encodeURIComponent(userId)}`, {
+							headers: { "Content-type": "application/json" }
+						})
+						.then(r => { if (!r.ok) throw new Error(r.status); return r.json(); })
+						.then(data => {
+							if(data.available){
+								console.log("로그인 진행");
+								
+							} else{
+								console.log('회원가입 진행');
+								signup(user);
+							}
+						}*/
+						
+						
 						
 					},
 					fail : function(response){
@@ -73,5 +97,9 @@ window.addEventListener('DOMContentLoaded', () => {
 		kakaoLogout();
 		console.log("로그아웃");
 	});
+	
+	function signup(user){
+		fetch('/member/kakaosignup.me')
+	}
 	
 });
