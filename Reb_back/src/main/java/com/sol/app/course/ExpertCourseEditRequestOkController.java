48f5d08a736974764a26041e07710919e4aeb315
boraJun <qhfk7751@kakao.com>
2025-09-22 03:37:02 +0900

    250922 03:36 전보라 수업파일 최대 크기 변경

--- Reb_back/src/main/java/com/sol/app/course/ExpertCourseEditRequestOkController.java
+++ Reb_back/src/main/java/com/sol/app/course/ExpertCourseEditRequestOkController.java
@@ -49,7 +49,7 @@ public class ExpertCourseEditRequestOkController implements Execute {
 
 			// 파일 업로드 환경 설정
 			final String UPLOAD_PATH = request.getSession().getServletContext().getRealPath("/") + "upload/";
-			final int FILE_SIZE = 1024 * 1024 * 5; // 5MB
+			final int FILE_SIZE = 1024 * 1024 * 50; // 50MB
 			// MultipartRequest를 이용한 데이터 파싱
 			MultipartRequest multipartRequest = new MultipartRequest(request, UPLOAD_PATH, FILE_SIZE, "utf-8",
 					new DefaultFileRenamePolicy());
