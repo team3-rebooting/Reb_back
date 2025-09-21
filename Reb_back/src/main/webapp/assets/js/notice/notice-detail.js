6b8f6de20eacb52c6f4277d773e24f6ce5c79ea7
jh011228 <jh01122844@gmail.com>
2025-09-22 03:31:30 +0900

    250922 03:30 최장현 공지사항 날짜 수정

--- Reb_back/src/main/webapp/assets/js/notice/notice-detail.js
+++ Reb_back/src/main/webapp/assets/js/notice/notice-detail.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const backButton = document.querySelector(".back-list");
+  const back = document.querySelector(".p-back");
 
-  backButton.addEventListener("click", function () {
-    // 브라우저 뒤로 가기
-    window.history.back();
-
-    // 또는 특정 페이지로 이동하려면 아래 사용
-    // window.location.href = "./../../app/notice/notice-list.html";
-  });
+  back.addEventListener("click", () => {
+  		history.back();
+  	});
 });
